eb5b76c333c08ed6369980b92b15f4df11f4d0f8
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2024-10-29 23:29:40 -0300

    Download e Executável.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ executables = [cx_Freeze.Executable('app.py')]
 cx_Freeze.setup(
     name="dino game",
     options={'build_exe': {'packages':['pygame'],
-                           'include_files':['imgs', 'charsprite']}},
+                           'include_files':['./imgs', './charsprite' , './audio']}},
 
     executables = executables
     
